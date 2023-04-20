ea80159c653335b88871ecdee6f93a8c393aac13
Felipe12002 <correolukas1604@gmail.com>
2023-04-19 22:47:04 -0400

    clase-dos

---
+++ clase-dos/hola.py
@@ -0,0 +1,13 @@
+print("Hola Mundo")
+print("Esto es una nueva linea")
+
+edad = 50
+sueldo = 1000.50
+nombre = "Felipe"
+isTeacher = True
+
+print(edad, sueldo, nombre, isTeacher)
+
+numero_1 = 1
+numero_2 = 3
+print({numero_1+numero_2})
